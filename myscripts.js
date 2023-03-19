c4b93fa2def5c6600025a58048e491eb1f11ab0c
Philip Gallagher <Pgallagher93@hotmail.com>
2023-03-19 14:16:05 +0000

    playRound function completed

--- myscripts.js
+++ myscripts.js
@@ -13,8 +13,50 @@ function playerSelection(){
     else return console.log("Invalid Player Choice");
     
 }
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, getComputerChoice) {
     let playerChoice = playerSelection;
-    let getComputerChoice = computerSelection;
-    
+    let ComputerChoice = getComputerChoice;
+
+    if (playerChoice === "rock"){
+        switch(ComputerChoice){
+            case "rock":
+                console.log("It's a draw!")
+                break;
+            case "paper":
+                console.log("You lose! Paper beats Rock")
+                break;
+            case "scissors":
+                console.log("You win! Rock beats Scissors")
+                break;
+        }
+    }
+
+    if (playerChoice === "scissors"){
+        switch(ComputerChoice){
+            case "scissors":
+                console.log("It's a draw!")
+                break;
+            case "rock":
+                console.log("You lose! Rock beats Scissors")
+                break;
+            case "paper":
+                console.log("You win! Scissors beats Paper")
+                break;
+        }
+    }
+
+    if (playerChoice === "paper"){
+        switch(ComputerChoice){
+            case "paper":
+                console.log("It's a draw!")
+                break;
+            case "scissors":
+                console.log("You lose! Scissors beats Paper")
+                break;
+            case "rock":
+                console.log("You win! Paper beats Rock")
+                break;
+        }
+    }
+
 }
