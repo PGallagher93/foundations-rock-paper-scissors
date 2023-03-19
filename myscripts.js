77c9820e3771a2b4f71b59f1bde8dc76923bcca0
Philip Gallagher <Pgallagher93@hotmail.com>
2023-03-19 14:01:31 +0000

    player selection function added

--- myscripts.js
+++ myscripts.js
@@ -4,3 +4,17 @@ function getComputerChoice() {
     let choice = choices[randomIndex];
     return choice;
 }
+
+function playerSelection(){
+    let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors"){
+        return playerChoice;
+    }
+    else return console.log("Invalid Player Choice");
+    
+}
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection;
+    let getComputerChoice = computerSelection;
+    
+}
